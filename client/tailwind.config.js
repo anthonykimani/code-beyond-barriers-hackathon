6cd264d62c4b5662f374b85cf33db40f0a2a1bb8
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2023-01-17 13:39:19 +0300

    Add Navbar to the website to help users navigate

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        button: "#5FB868",
+      },
+      colors: {
+        text: "#5FB868",
+      },
+      fontSize: {},
+      height: {},
+      width: {},
+    },
+
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
