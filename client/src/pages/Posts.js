5f627bcee2a34207a035237240e819467a372f41
anthonykimani <kimaniantoni@gmail.com>
2023-01-21 09:20:10 +0300

    add test

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -5,20 +5,76 @@ import Post from "../components/Post";
 import { AppContext } from "../contexts/AppContext";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState({
+    "count": 3,
+    "results": [
+        {
+            "id": 1,
+            "url": "https://inuasauti.api.ushahidi.io/api/v3/posts/1",
+            "user": {
+                "id": 2,
+                "url": "https://inuasauti.api.ushahidi.io/api/v3/users/2"
+            },
+            "parent_id": null,
+            "form": {
+                "id": 1,
+                "url": "https://inuasauti.api.ushahidi.io/api/v3/forms/1"
+            },
+            "message": null,
+            "color": null,
+            "type": "report",
+            "title": "inua test",
+            "slug": "inua-test",
+            "content": "this is a test",
+            "status": "published",
+            "created": "2023-01-16T15:38:02+00:00",
+            "updated": null,
+            "locale": "en_us",
+            "values": {
+                "location_default": [
+                    {
+                        "lon": 36.805026252977,
+                        "lat": -1.306504499738
+                    }
+                ]
+            },
+            "post_date": "2023-01-16T15:36:59+00:00",
+            "tags": [],
+            "published_to": [],
+            "completed_stages": [],
+            "sets": [],
+            "lock": null,
+            "source": null,
+            "contact": null,
+            "data_source_message_id": null,
+            "allowed_privileges": [
+                "read",
+                "search"
+            ]
+        }
+    ],
+    "limit": null,
+    "offset": 0,
+    "order": "desc",
+    "orderby": "post_date",
+    "curr": "https://inuasauti.api.ushahidi.io/api/v3/posts?orderby=post_date&order=desc&offset=0&limitPosts=1",
+    "next": "https://inuasauti.api.ushahidi.io/api/v3/posts?orderby=post_date&order=desc&offset=0&limitPosts=1",
+    "prev": "https://inuasauti.api.ushahidi.io/api/v3/posts?orderby=post_date&order=desc&offset=0&limitPosts=1",
+    "total_count": 3
+});
   const [info, setInfo] = useState([]);
-  const [title, setTitle] = useState(null);
-  const [postDate, setPostDate] = useState(null);
-  const [category, setCategory] = useState(null);
-  const [source, setSource] = useState(null);
-  const [message, setMessage] = useState(null);
+  const [title, setTitle] = useState("nakuru");
+  const [postDate, setPostDate] = useState("2023-01-16T15:36:59+00:00");
+  const [category, setCategory] = useState("land dispute");
+  const [source, setSource] = useState("twitter");
+  const [message, setMessage] = useState("land grabbing in nakuru");
 
   const { contract, kit, useAccount, notification, connectWallet } =
     useContext(AppContext);
 
   //TODO
   const writePost = async () => {
-    const params = [category, title, source, postDate, message, category];
+    const params = [category, title, source, postDate, message ];
     try {
       await contract.methods
         .getMessagefromUshahidiApi(...params)
@@ -68,25 +124,34 @@ const Posts = () => {
   }, []);
   
   const setPostData = async()=>{
-    await console.log(posts.results[0].content);
+    
+    // await console.log(posts.results[0].content);
     // await setMessage(posts.results[0].content);
-    await  setMessage(posts.results[0].content);
-    await  setSource(posts.results[0].source);
-    await  setTitle(posts.results[0].title);
-    await  setPostDate(posts.results[0].post_date);
+    if(posts.results.length > 1 ){
+      await  setMessage(posts.results[0].content);
+      await  setSource(posts.results[0].source);
+      await  setTitle(posts.results[0].title);
+      await  setPostDate(posts.results[0].post_date);
+      console.log("new data found");
+      writePost();
+    }
+    else{
+      console.log("no new data");
+    }
   }
+
+  // console.log(posts.results.length);
   
   useEffect(()=>{
     
-    console.log(posts?.results?.[0]);
+    // console.log(posts?.results?.[0]);
     setPostData();
-    console.log(posts.results[0].title);
-    console.log(posts.results[0].content);
-    console.log(posts.results[0].source);
-    console.log(posts.results[0].post_date);
+    // console.log(title);
+    // console.log(posts.results[0].content);
+    // console.log(posts.results[0].source);
+    // console.log(posts.results[0].post_date);
     },[posts])
     
-    // writePost();
 
 
   return (
